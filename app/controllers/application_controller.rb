53c3e32e182f839bdd9c677f1b412c129c9f5fdc
Devops <devops@kenscio.com>
2025-01-07 15:31:36 +0530

    changed the task def and docker compose file for /box sandboxing environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
-class ApplicationController < SessionsController
-  private
-
-  def pagination_dict(collection)
-    {
-      current_page: collection.current_page,
-      next_page: collection.next_page,
-      prev_page: collection.previous_page,
-      total_pages: collection.total_pages,
-      total_count: collection.total_entries
-    }
+class ApplicationController < ActionController::Base
+  def hello_world
+    render json: { message: "Hello World" }, status: :ok
   end
 end
